c16edece14c6c309e1b084f0bc381c98d88d848d
Peter Hewett <16981266+hewettp@users.noreply.github.com>
2019-01-15 21:00:23 +1300

    converted to python3

--- GrampsCrossing.py
+++ GrampsCrossing.py
@@ -1,19 +1,20 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 #
 # Peter Hewett, Filipe Correia
-# Copyright GPL 2012 - 2018
+# Copyright GPL 2012 - 2019
 # edited: 10 March 2014
 # edited: V0.4 18th August 2014, Maurice Snell:
 #	Made Windows compatible, not restested on *nix but should still work.
 #	Increased & clarified output messages
 #	Fixed bug with writing output when user cancels optimisation.
+# edited: 15 Jan 2019 PWH: converted to python3
 #
 # reorder Gramps dot file to minimise crossings in relationship chart
 # usage:
 #    create a relationship graph in Gramps to produce .gv file
 #    copy .gv file and this .py file to the same directory
 #    in that directory, run
-#      $./GrampsCrossing0.6.py yourfile.gv
+#      $./GrampsCrossing.py yourfile.gv
 #    output is now more verbose:
 #        iterations, span, index, nr_cross_best, nr_cross_new, iteration time
 #    it takes several minutes, depending on file size
@@ -25,36 +26,40 @@ import math
 import subprocess
 import sys
 import signal
-import platform
 import time
 import os
 
 dot_markup = ""
 
+
 def signal_handler(thesignal, frame):
-    print 'Ctrl+C pressed! Writing files and aborting...'
+    print('Ctrl+C pressed! Writing files and aborting...')
     write_files(dot_markup)
     sys.exit(0)
+
+
 signal.signal(signal.SIGINT, signal_handler)
 
+
+# read the dot file and parse into parts
 def parse():
     with open(sys.argv[1], 'r') as f:
-        src=f.readlines()
+        src = f.readlines()
     f.close()
 
     # find end of header section
-    for i,l in enumerate(src):
-        i=i+1    
+    for i, l in enumerate(src):
         if 'node ' in l:
+            p1 = i + 2
             break
 
     # parse input file
-    dot_header=src[0:i+1]
-    dot_body=src[i+1:]
-    dot_links=[]
-    dot_people=[]
-    dot_spouses=[]
-    dot_families=[]
+    dot_header = src[0:p1]
+    dot_body = src[p1:]
+    dot_links = []
+    dot_people = []
+    dot_spouses = []
+    dot_families = []
 
     j = 0
     while j < len(dot_body):
@@ -64,10 +69,10 @@ def parse():
         elif l.startswith('  I') or l.startswith('  "I'):
             dot_people.append(l)
         elif l.startswith('  subgraph'):
-            for y,k in enumerate(dot_body[j:]):
+            for y, k in enumerate(dot_body[j:]):
                 if k.startswith('  }'):
                     break
-            dot_spouses.append(''.join(dot_body[j:j+y+1]))
+            dot_spouses.append(''.join(dot_body[j:j + y + 1]))
             j += y
         else:
             dot_families.append(l)
@@ -75,82 +80,86 @@ def parse():
 
     return dot_header, dot_people, dot_families, dot_spouses, dot_links
 
+
 # function to return number of crossings for given dot file
-def crossings(df, output_files=False):    
+def crossings(df, output_files=False):
     if output_files:
-        print "Generating output DOT."
+        print("Generating output DOT.")
         cmd = 'dot -v -Tpdf > gcf1.pdf'
-        f=open('gcf1.dot', 'w')
+        f = open('gcf1.dot', 'w')
         f.write(''.join(df))
         f.close()
-        print "Generating output PDF."
+        print("Generating output PDF.")
     else:
-        cmd = 'dot -v -Tpdf > ' + os.devnull # silent, now more portable including Windows
-    #print "cmd:",cmd
+        cmd = 'dot -v -Tpdf > ' + os.devnull  # silent, now more portable including Windows
 
     result = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
     try:
-        result.stdin.write(''.join(df))
+        result.stdin.write(''.join(df).encode())
         result.stdin.close()
-    except IOError, e:
-        print "Exception raised in subprocess call"
-        print e
-        print ''.join(list(result.stderr))
+    except IOError as e:
+        print("Exception raised in subprocess call")
+        print(e)
+        print(''.join(list(result.stderr)))
 
     for l in result.stderr:
-        if 'crossings' in l:
+        if 'crossings' in l.decode():
             break
-
-    p1 = l.find(':')
-    p2 = l.find('crossings')
+    line = l.decode()
+    p1 = line.find(':')
+    p2 = line.find('crossings')
     if not output_files:
         # On Windows this was causing memory issues by leaving multiple instances hanging.
         result.terminate()
-    return int(l[p1+1:p2-1])
+    return int(line[p1 + 1:p2 - 1])
+
 
 def write_files(dot_markup):
     crossings(dot_markup, output_files=True)
-    print "Crossings reduced from",nr_cross_original,"to",nr_cross_best,"runtime=%0.3f seconds"%(time.time()-startTime),"iterations=%d"%iterations
-    print "Average iteration time=%0.3f seconds"%(totalTime/iterations),"longest iteration time=%0.3f seconds"%longestTime,"shortest iteration time=%0.3f seconds"%shortestTime
+    print("Crossings reduced from", nr_cross_original, "to", nr_cross_best,
+          "runtime=%0.3f seconds" % (time.time() - startTime), "iterations=%d" % iterations)
+    print("Average iteration time=%0.3f seconds" % (totalTime / iterations),
+          "longest iteration time=%0.3f seconds" % longestTime, "shortest iteration time=%0.3f seconds" % shortestTime)
 
-if __name__=="__main__":
-    startTime=time.time()
+
+if __name__ == "__main__":
+    startTime = time.time()
     header, people, families, spouses, links = parse()
 
-    nsize=len(people)
-    span0 = int(math.log(nsize-1)/math.log(2))
-    span=2**span0
+    nsize = len(people)
+    span = int(2 ** int(math.log(nsize - 1) / math.log(2)))
 
-    new_people=[]
+    new_people = []
     nr_cross_best = crossings(header + people + links + spouses + families)
-    print "people="+str(nsize), "span0="+str(span0), "initial crossings =",nr_cross_best
+    print("people=" + str(nsize), "initial span=" + str(span), "initial crossings =" + str(nr_cross_best))
     nr_cross_new = nr_cross_best
     nr_cross_original = nr_cross_best
-    iterations=0
-    totalTime=0
-    longestTime=0
-    shortestTime=float("inf")
+    iterations = 0
+    totalTime = 0
+    longestTime = 0
+    shortestTime = float("inf")
     while span >= 1:
-        for i in range(nsize-span):
-            iterationsStartTime=time.time()
+        for i in range(nsize - span):
+            iterationsStartTime = time.time()
             new_people = people[:]
             tmp = new_people[i]
-            new_people[i] = new_people[i+span]
-            new_people[i+span] = tmp
-            dot_markup=header + new_people + links + spouses + families
+            new_people[i] = new_people[i + span]
+            new_people[i + span] = tmp
+            dot_markup = header + new_people + links + spouses + families
             nr_cross_new = crossings(dot_markup)
-            iterationTime=time.time()-iterationsStartTime
-            if iterationTime<shortestTime:
-                shortestTime=iterationTime
-            if iterationTime>longestTime:
-                longestTime=iterationTime
-            totalTime+=iterationTime
-            iterations+=1
-            print "iterations=%d span=%d i=%d nr_cross_best=%d nr_cross_new=%d time=%0.3fs" % (iterations, span, i, nr_cross_best, nr_cross_new, iterationTime)
+            iterationTime = time.time() - iterationsStartTime
+            if iterationTime < shortestTime:
+                shortestTime = iterationTime
+            if iterationTime > longestTime:
+                longestTime = iterationTime
+            totalTime += iterationTime
+            iterations += 1
+            print("iterations=%d span=%d i=%d nr_cross_best=%d nr_cross_new=%d time=%0.3fs" % (
+            iterations, span, i, nr_cross_best, nr_cross_new, iterationTime))
             if nr_cross_new < nr_cross_best:
                 people = new_people[:]
-                nr_cross_best=nr_cross_new
+                nr_cross_best = nr_cross_new
             if nr_cross_best == 0:
                 break
-        span = span/2
+        span = int(span / 2)
     write_files(header + people + links + spouses + families)
